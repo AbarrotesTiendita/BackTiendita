19c1e63da9beb3b9707f311cc137ef030d9ef445
AbarrotesTiendita <abarroteslatiendita12@gmail.com>
2023-03-10 09:11:33 -0600

    agregue los errores

--- src/controllers/vendedores.controller.js
+++ src/controllers/vendedores.controller.js
@@ -1,51 +1,73 @@
 import { pool } from '../db.js'
 
 export const getVendedores = async (req, res) => {
+    try {
     const [rows] = await pool.query('SELECT * FROM vendedor')
     res.json(rows)
+    } catch (error) {
+        return res.status(500).json({
+            message:'Algo salio mal'
+        })
+    }
 }
 
 export const getVendedor = async (req, res) => {
+    try {
     const [rows] = await pool.query('SELECT * FROM vendedor WHERE idVendedor = ?', [req.params.id])
-    
     if (rows.length <= 0) return res.status(404).json({
         message: 'Vendedor no encontrado'
     })
-
     res.json(rows)
+    } catch (error) {
+        return res.status(500).json({
+            message:'Algo salio mal'
+        })
+    }
 }
 
 export const postVendedores = async (req, res) => {
     const {Nom_Vendedor, Contraseña} = req.body
+    try {
     const [rows] = await pool.query('INSERT INTO vendedor (Nom_Vendedor, Contraseña) VALUES (?, sha1(?))', [Nom_Vendedor, Contraseña])
     res.send({
         id: rows.insertId,
         Nom_Vendedor,
         Contraseña
     })
+    } catch (error) {
+        return res.status(500).json({
+            message:'Algo salio mal'
+        })
+    }
 } 
 
 export const putVendedores = async (req, res) => {
     const {idVendedor} = req.params
     const {Nom_Vendedor, Contraseña} = req.body
-
+    try {
     const [result] = await pool.query('UPDATE vendedor SET Nom_Vendedor = IFNULL(?, Nom_Vendedor), Contraseña = IFNULL(sha1(?), Contraseña) WHERE idVendedor = ?', [Nom_Vendedor, Contraseña, idVendedor])
-
     console.log(result)
-
     if(result === 0) return res.status(404).json({
         message:'Vendedor no actualizado'
     })
-
     res.json('Actualizado')
+    } catch (error) {
+        return res.status(500).json({
+            message:'Algo salio mal'
+        })
+    }
 }
 
 export const deleteVendedores = async (req, res) => {
+    try {
     const result = await pool.query('DELETE FROM vendedor WHERE idVendedor = ?', [req.params.id])
-    
     if (result.affectedRows <= 0) return res.status(404).json({
         message: 'Vendedor no encontrado'
     })
-
     res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message:'Algo salio mal'
+        }) 
+    }
 }
--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -1,6 +1,12 @@
 import { pool } from '../db.js'
 
 export const getActuales = async (req, res) => {
+    try {
     const [rows] = await pool.query('SELECT SUM(Total) AS Ganancias FROM venta')
     res.json(rows)
+    } catch (error) {
+        return res.status(500).json({
+            message:'Algo salio mal'
+        })
+    }
 }
