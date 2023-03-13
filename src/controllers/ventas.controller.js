01ad5ff4c958e942842ef483c7192a5e50f4987c
ThankedDeer <eduardo.salinas12@hotmail.com>
2023-03-12 19:07:54 -0600

    se agrega were en api de ganancias

--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -2,7 +2,7 @@ import { pool } from '../db.js'
 
 export const getActuales = async (req, res) => {
     try {
-    const [rows] = await pool.query('SELECT SUM(Total) AS Ganancias FROM venta')
+    const [rows] = await pool.query('SELECT SUM(Total) AS Ganancias FROM venta WHERE fecha_hora = GETDATE()')
     res.json(rows)
     } catch (error) {
         return res.status(500).json({
