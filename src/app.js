0664467adf414ed3fb1121120a8989500f6a7a2f
AbarrotesTiendita <abarroteslatiendita12@gmail.com>
2023-03-14 13:32:49 -0600

    quite login

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import productosRoutes from './routes/productos.routes.js'
 import categoriaRoutes from './routes/categoria.routes.js'
 import indexRoutes from './routes/index.routes.js'
 import ventasRoutes from './routes/ventas.routes.js'
-import loginRoutes from './routes/login.routes.js'
 
 import {PORT} from './config.js'
 
@@ -16,7 +15,6 @@ app.use('/api',vendedoresRoutes)
 app.use('/api', productosRoutes)
 app.use('/api', categoriaRoutes)
 app.use('/api', ventasRoutes)
-app.use('/api', loginRoutes)
 app.use(indexRoutes)
 
 export default app
