d65e6a38cfdc983adfb5ca266ea380e99beb556b
Yaelox <yaelsilvestre64@gmail.com>
2023-03-17 09:50:51 -0600

    Holus

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,8 +3,6 @@ import { postUsuarios } from '../controllers/usuarios.controller.js'
 
 const router = Router()
 
-router.get('/usuarios', getUsuarios)
-
 router.post('/usuarios', postUsuarios)
 
 export default router
