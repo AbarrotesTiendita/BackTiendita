5f4a2067818a64e055c9ca7fb1c1e0edf7e51e49
Yaelox <yaelsilvestre64@gmail.com>
2023-03-19 11:25:29 -0600

    jjv

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,17 +1,17 @@
 import { pool } from '../db.js'
 
-export const postUsuarios = async (req, res, next) => {
+export const postUsuarios = async (req, resp, next) => {
     const reqData={};
-    var username = re.body.user;
-    var password = re.body.pass;
+    var username = req.body.user;
+    var password = req.body.pass;
     
-    database.query('select * from vendedor where Nom_Vendedor=? and Contraseña=sha1(?)',[username,password],(err,rows,field)=>{
+    database.query('select * from vendedor where Nom_Vendedor =? and Contraseña =sha1(?)',[username,password],(err,rows,field)=>{
         console.log(rows);
         if(!err){
             const hash=crypto.createHash('sha1').update(password).digest('hex');
-            if(rows.leghth == 1 && rows[0].Nom_Vendedor == username && rows[0].Contraseña== hash){
+            if(rows.length == 1 && rows[0].Nom_Vendedor == username && rows[0].Contraseña== hash){
                 const user = rows[0];
-                jwt.sign({id: vendedor.idVendedor}, config.secret,{expiresIn:"24h"},(err,token)=>{
+                jwt.sign({id: user.idVendedor}, config.secret,{expiresIn:"24h"},(err,token)=>{
                     resp.json({token: token})
                 });
             }
