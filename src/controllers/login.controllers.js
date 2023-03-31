3058c7a1aa6c73bab1947db07dd17ab2c066981a
Yaelox <yaelsilvestre64@gmail.com>
2023-03-30 20:47:30 -0600

    PTM

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -3,39 +3,29 @@ import { pool } from "../db.js";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 
-export const login = async (req, resp) => {
-  var Nom_Vendedor = req.body.Nom_Vendedor;
-  var Contraseña = req.body.Contraseña;
+export const login = (req, resp) => {
+  var username = req.body.Nom_Vendedor;
+  var pass = req.body.Contraseña;
 
-  pool.query(
-    "select * from vendedor where Nom_Vendedor = ? and Contraseña = sha1(?)",
-    [Nom_Vendedor, Contraseña],
-    (err, rows) => {
-      console.log(rows);
-      if (!err) {
-        const hash = crypto.createHash("sha1").update(Contraseña).digest("hex");
-        if (
-          rows.length == 1 &&
-          rows[0].Nom_Vendedor == Nom_Vendedor &&
-          rows[0].Contraseña == hash
-        ) {
-          const user = rows[0];
-          jwt.sign(
-            { user: user },
-            "accessKey",
-            (err, token) => {
-              resp.json({ token: token });
-            }
-          );
-        } else {
-          resp.sendStatus(403);
-        }
-      } else {
-        resp.sendStatus(503);
+  pool.query("select * from vendedor where Nom_Vendedor=? and Contraseña = sha1(?)",[username,pass],(err,rows,fields)=>{
+    console.log(rows);
+    if(!err){
+      const hash = crypto.createHash("sha1").update(pass).digest("hex");
+      if(rows.lenght == 1 && rows[0].Nom_Vendedor == username && rows[0].Contraseña == hash){
+        const user = rows[0];
+        jwt.sign({user:user}, "accesKey", {expiresIn: "24h"}, (err,token)=>{
+          resp.json({token: token})
+        });
+      }
+      else{
+        resp.sendStatus(403);
       }
     }
-  );
-}; 
+    else{
+      resp.sendStatus(503);
+    }
+  });
+};
 
 export default login;
 
