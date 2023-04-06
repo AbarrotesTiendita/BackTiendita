e25eb4fb55d0f3a14fbba5914400e3e20f64062a
Axel <axelolmar@gmail.com>
2023-04-05 20:13:03 -0600

    si api actualizar categoria otra vez

--- src/routes/categoria.routes.js
+++ src/routes/categoria.routes.js
@@ -9,7 +9,7 @@ router.get('/categoria/:id', getCategoria)
 
 router.get('/categoriass/:id', getCategoriaa)
 
-router.patch('/categoriact/:id', putCategoriact)
+router.patch('/categoriact/:idCategoria', putCategoriact)
 
 router.post('/categorias', postCategorias)
 
