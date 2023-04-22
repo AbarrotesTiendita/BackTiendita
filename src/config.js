bbfe28bc143659baccfcc54a5d00f92ece93d0bc
ThankedDeer <eduardo.salinas12@hotmail.com>
2023-04-22 12:09:44 -0600

    ultimo commit del back end

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ config()
 
 export const PORT = process.env.PORT || 3000
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Eclatbmx1'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '211068'
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_DATABASE = process.env.DB_DATABASE || 'puntoventa'
 export const DB_PORT = process.env.DB_PORT || 3306
