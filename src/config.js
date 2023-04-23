87f5903d4b0c191f4582b725f40967902e47e122
Yaelox <yaelsilvestre64@gmail.com>
2023-04-22 21:01:47 -0600

    kschncdsk

--- src/config.js
+++ src/config.js
@@ -4,11 +4,7 @@ config()
 
 export const PORT = process.env.PORT || 3000
 export const DB_USER = process.env.DB_USER || 'root'
-<<<<<<< HEAD
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-=======
-export const DB_PASSWORD = process.env.DB_PASSWORD || '211068'
->>>>>>> 4bcf9a9cca7663b79f0fb8f22be995b6676511ea
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_DATABASE = process.env.DB_DATABASE || 'puntoventa'
 export const DB_PORT = process.env.DB_PORT || 3306
